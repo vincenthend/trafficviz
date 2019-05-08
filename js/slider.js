452a2cadcd8be94f05dc16de83d86248028a399f
Vincent Hendryanto Halim <13515089@std.stei.itb.ac.id>
2019-05-09 01:42:23 +0700

    Smoothed slider

--- js/slider.js
+++ js/slider.js
@@ -1,41 +1,64 @@
 function changeTimeLabel() {
 	var startTime = document.getElementById("time-slider").value;
+	if(startTime >= 0){
+		d3.select("body").transition().duration(150).styleTween("background-color",function(){
+			return d3.interpolateRgb(d3.select(this).style("background-color"), getBackgroundColor(startTime));
+		});
 
-	var body = document.body;
-	body.style.backgroundColor = getBackgroundColor(startTime);
+		var deg = parseInt(startTime) * 360 / 24;
 
-	var deg = parseInt(startTime) * 360 / 24;
+		d3.select("svg#DialImage #XMLID_308_")
+			.transition().duration(150)
+			.attrTween('transform', function(){
+				return d3.interpolateString(d3.select(this).attr('transform'),'rotate('+deg+',-121.5,397)')
+			});
 
-	var clock = document.getElementById("time-clock");
-	clock.style.transform = 'rotate('+deg+'deg)';
+		var finishTime = parseInt(startTime) + 1;
 
-	var finishTime = parseInt(startTime) + 1;
-
-	if(finishTime > 23) {
-		finishTime = 0;
+		// Setup time label
+		if(finishTime > 23) {
+			finishTime = 0;
+		}
+		if(startTime < 10) {
+			startTime = "0" + startTime;
+		}
+		if(finishTime < 10) {
+			finishTime = "0" + finishTime;
+		}
+		d3.select("#time-label").text(startTime + ":00-" + finishTime +":00");
+	} else {
+		// Reset to default position
+		d3.select("svg#DialImage #XMLID_308_")
+			.transition().duration(150)
+			.attrTween('transform', function(){
+				return d3.interpolateString(d3.select(this).attr('transform'),'rotate(0,-121.5,397)')
+			});		
+		animateDial();
+		d3.select("#time-label").text("All Time");
 	}
-
-	if(startTime < 10) {
-		startTime = "0" + startTime;
-	}
-
-	if(finishTime < 10) {
-		finishTime = "0" + finishTime;
-	}
-
-	document.getElementById("time-label").innerHTML = startTime + ":00-" + finishTime +":00";
 }
 
+function animateDial(){
+	d3.select("svg#DialImage #XMLID_308_")
+		.transition().duration(5000)
+		.attrTween('transform', function(){
+			return d3.interpolateString('rotate(0,-121.5,397)','rotate(180,-121.5,397)')
+		}).transition().duration(5000)
+		.attrTween('transform', function(){
+			return d3.interpolateString('rotate(180,-121.5,397)','rotate(0,-121.5,397)')
+		})
+		.on('end', animateDial);
+}
 
 function getBackgroundColor(startTime) {
 	var backgroundGradient = new Rainbow();
 	
 	if(startTime <= 11) {
 		backgroundGradient.setNumberRange(0, 11);
-		backgroundGradient.setSpectrum('#2C3E50', '#FD746C');
+		backgroundGradient.setSpectrum('#1C242B', '#5CB2CC');
 	} else {
 		backgroundGradient.setNumberRange(12, 23);
-		backgroundGradient.setSpectrum('#FD746C', '#2C3E50');
+		backgroundGradient.setSpectrum('#5CB2CC', '#1C242B');
 	}
 	return "#" + backgroundGradient.colourAt(startTime);
 }
